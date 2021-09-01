5cd682e2b9100988709c9869280adc8a2cbf7583
v_shunlzhu <v_shunlzhu@tencent.com>
2021-09-01 15:12:35 +0800

    vue3两种方式搭建工程

---
+++ vue3_test_vite/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- vue3中template不用只局限于一个标签下,可以有多个标签在外层,在vue2中是必须只有一个最外层标签包裹 -->
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- <div>哈哈哈</div> -->
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ vue3_test_vite/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pages: {
+    index: {
+      // 入口文件
+      entry: 'src/main.js'
+    }
+  },
+  lintOnSave: false // 关闭语法检查
+}
